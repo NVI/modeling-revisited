e2a851ff5800072839216ab084dba62abc39f1b1
Niko Ilomäki <niko.ilomaki@gmail.com>
2013-04-11 16:47:08 +0300

    Finland->eduskunta

--- main.cpp
+++ main.cpp
@@ -29,23 +29,23 @@ int main(int argc, char **argv) {
 
     Setup setup;
 
-    System Finland;
+    System eduskunta;
     double vihr [8] = {0.000, 0.254, 0.309, 0.267, 0.409, 0.425, 0.366, 0.570};
-    Finland.addParty(setup.getLambda(), std::vector<double> (vihr, vihr + sizeof (vihr) / sizeof (double))); // Vihreat, index=0
+    eduskunta.addParty(setup.getLambda(), std::vector<double> (vihr, vihr + sizeof (vihr) / sizeof (double))); // Vihreat, index=0
     double vas [8] = {0.254, 0.000, 0.164, 0.435, 0.533, 0.504, 0.368, 0.538};
-    Finland.addParty(setup.getLambda(), std::vector<double> (vas, vas + sizeof (vas) / sizeof (double))); // Vasemmistoliitto, index=1
+    eduskunta.addParty(setup.getLambda(), std::vector<double> (vas, vas + sizeof (vas) / sizeof (double))); // Vasemmistoliitto, index=1
     double sdp [8] = {0.309, 0.164, 0.000, 0.376, 0.432, 0.382, 0.227, 0.377};
-    Finland.addParty(setup.getLambda(), std::vector<double> (sdp, sdp + sizeof (sdp) / sizeof (double))); // SDP, index=2
+    eduskunta.addParty(setup.getLambda(), std::vector<double> (sdp, sdp + sizeof (sdp) / sizeof (double))); // SDP, index=2
     double rkp [8] = {0.267, 0.435, 0.376, 0.000, 0.151, 0.200, 0.251, 0.418};
-    Finland.addParty(setup.getLambda(), std::vector<double> (rkp, rkp + sizeof (rkp) / sizeof (double))); // RKP, index=3
+    eduskunta.addParty(setup.getLambda(), std::vector<double> (rkp, rkp + sizeof (rkp) / sizeof (double))); // RKP, index=3
     double kok [8] = {0.409, 0.533, 0.432, 0.151, 0.000, 0.090, 0.233, 0.329};
-    Finland.addParty(setup.getLambda(), std::vector<double> (kok, kok + sizeof (kok) / sizeof (double))); // Kokoomus, index=4
+    eduskunta.addParty(setup.getLambda(), std::vector<double> (kok, kok + sizeof (kok) / sizeof (double))); // Kokoomus, index=4
     double kesk [8] = {0.425, 0.504, 0.382, 0.200, 0.090, 0.000, 0.163, 0.240};
-    Finland.addParty(setup.getLambda(), std::vector<double> (kesk, kesk + sizeof (kesk) / sizeof (double))); // Keskusta, index=5
+    eduskunta.addParty(setup.getLambda(), std::vector<double> (kesk, kesk + sizeof (kesk) / sizeof (double))); // Keskusta, index=5
     double kd [8] = {0.366, 0.368, 0.227, 0.251, 0.233, 0.163, 0.000, 0.204};
-    Finland.addParty(setup.getLambda(), std::vector<double> (kd, kd + sizeof (kd) / sizeof (double))); // Kristillisdemokraatit, index=6
+    eduskunta.addParty(setup.getLambda(), std::vector<double> (kd, kd + sizeof (kd) / sizeof (double))); // Kristillisdemokraatit, index=6
     double ps [8] = {0.570, 0.538, 0.377, 0.418, 0.329, 0.240, 0.204, 0.000};
-    Finland.addParty(setup.getLambda(), std::vector<double> (ps, ps + sizeof (ps) / sizeof (double))); // Perussuomalaiset, index=7
+    eduskunta.addParty(setup.getLambda(), std::vector<double> (ps, ps + sizeof (ps) / sizeof (double))); // Perussuomalaiset, index=7
 
     // Boost setup
     boost::random::mt19937 prng;
@@ -67,7 +67,7 @@ int main(int argc, char **argv) {
     Bundle bundle;
     bundle.push_back(network);
 
-    Project project(filenames, setup, Finland, bundle);
+    Project project(filenames, setup, eduskunta, bundle);
 
     std::cout << "Initialization done" << std::endl;
 
